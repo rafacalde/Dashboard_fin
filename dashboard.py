80a6ce2ceeb14deb5bd4b0be1926eef78669d654
rafacalde <rafacalde7@gmail.com>
2025-05-23 13:16:07 -0600

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -65,8 +65,8 @@ sheet = spreadsheet.worksheet("datos_paciente")
 # -------------------- CARGA DE DATOS CSV DESDE DRIVE --------------------
 @st.cache_data
 def cargar_datos():
-    file_id = "1joOz_ZxUDgfZZ-r3F0XjdNsUoSmXJEBg"  # Tu ID de archivo
-    url = f"https://drive.google.com/file/d/1joOz_ZxUDgfZZ-r3F0XjdNsUoSmXJEBg/view?usp=sharing"
+    file_id = "1joOz_ZxUDgfZZ-r3F0XjdNsUoSmXJEBg"
+    url = f"https://drive.google.com/uc?export=download&id={file_id}"
     df = pd.read_csv(url)
 
     # Verificamos si la columna 'Fecha' existe antes de convertirla
